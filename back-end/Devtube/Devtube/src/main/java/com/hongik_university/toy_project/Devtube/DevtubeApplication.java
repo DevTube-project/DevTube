fc47737a850d1000a8d5d8ffc2cd3e11483057c5
jmd5314 <jmd5314@gmail.com>
2024-01-18 21:32:48 +0900

    환경설정

---
+++ back-end/Devtube/Devtube/src/main/java/com/hongik_university/toy_project/Devtube/DevtubeApplication.java
@@ -0,0 +1,13 @@
+package com.hongik_university.toy_project.Devtube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevtubeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevtubeApplication.class, args);
+	}
+
+}
---
+++ back-end/Devtube/Devtube/src/test/java/com/hongik_university/toy_project/Devtube/DevtubeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hongik_university.toy_project.Devtube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevtubeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
