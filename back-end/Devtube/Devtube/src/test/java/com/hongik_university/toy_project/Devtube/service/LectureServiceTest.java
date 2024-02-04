5c2f5c8da62b2b6c184bbddeac79c89d7408b42e
jmd5314 <jmd5314@gmail.com>
2024-02-04 22:49:39 +0900

    강의 저장 테스트 코드 작성

---
+++ back-end/Devtube/Devtube/src/test/java/com/hongik_university/toy_project/Devtube/service/LectureServiceTest.java
@@ -0,0 +1,33 @@
+package com.hongik_university.toy_project.Devtube.service;
+
+import com.hongik_university.toy_project.Devtube.domain.BigField;
+import com.hongik_university.toy_project.Devtube.domain.Lecture;
+import com.hongik_university.toy_project.Devtube.domain.SmallField;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+class LectureServiceTest {
+    @Autowired
+    private LectureService lectureService;
+    @Test
+    public void 강의저장(){
+
+        String url = "https://www.googleapis.com/youtube/v3/playlists?part=snippet&id=PLuHgQVnccGMCeAy-2-llhw3nWoQKUvQck&key=AIzaSyBlAPO-tHgidphqbZQoc6vzaH4CdryTsX8";
+        Long id = lectureService.save(url, BigField.BASIC, SmallField.JAVA);
+        Lecture lecture = lectureService.findById(id);
+        String title = "Java 입문 수업 (생활코딩)";
+        System.out.println("lecture id = "+lecture.getLectureId());
+        System.out.println("lecture title = "+lecture.getTitle());
+        System.out.println("lecture ImageUrl = " + lecture.getImageUrl());
+        System.out.println("lecture channel = "+lecture.getChannelTitle());
+        System.out.println("lecture url = "+lecture.getUrl());
+        Assertions.assertEquals(lecture.getTitle(), title);
+    }
+}
