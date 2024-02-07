f4c356e0a039e0357a12e92df190f71cbae6ec97
jmd5314 <jmd5314@gmail.com>
2024-02-07 21:45:36 +0900

    회원가입 로직 구현

---
+++ back-end/Devtube/Devtube/src/main/java/com/hongik_university/toy_project/Devtube/domain/dto/UsersJoinRequestDto.java
@@ -0,0 +1,17 @@
+package com.hongik_university.toy_project.Devtube.domain.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class UsersJoinRequestDto {
+    private String userId;
+    private String password;
+    private String name;
+    private String nickname;
+    private Integer age;
+    private String gender;
+    private String bigField;
+    private String smallField;
+}
